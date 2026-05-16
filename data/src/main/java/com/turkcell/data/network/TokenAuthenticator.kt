79c29d5fbfb072f982893d99ced2bdba4e798f45
halitklyc <halitenes.kalayci@gmail.com>
2026-05-16 11:04:58 +0300

    prior response

--- data/src/main/java/com/turkcell/data/network/TokenAuthenticator.kt
+++ data/src/main/java/com/turkcell/data/network/TokenAuthenticator.kt
@@ -9,23 +9,21 @@ import okhttp3.Request
 import okhttp3.Response
 import okhttp3.Route
 
-// Sadece HTTP 401'lerde çalış, Refresh akışı sürdür.
 class TokenAuthenticator(
     private val tokenStore: TokenStore,
     private val refreshApiProvider: () -> AuthApi,
 ) : Authenticator {
     override fun authenticate(route: Route?, response: Response): Request? {
-        // İstek 401'e düştüğü anda, eğer sistemde jwt-refresh pairi tanımlıysa git refresh ile yeni jwt alıp isteği tekrar dene.
+        // İsteğin tekrar tekrar buraya düşmesi -> refresh olsa bile 401 gelebilir
+        if(response.priorResponseCount() >= 1) return null
+
         val refreshToken = tokenStore.refreshTokenBlocking() ?: return null;
 
-        return synchronized(this) // lock
+        return synchronized(this)
         {
-            // Bu blokta birden fazla istek aynı anda 401 alırsa kuyruğa girer..
-            // Blok bitene kadar, yeni yapıları beklet..
             val current = tokenStore.accessTokenBlocking()
             val sentToken = response.request.header("Authorization")?.removePrefix("Bearer ")
 
-            // Başkası tarafından token değişmise, onu kullan ve devam et..
             if(current != null && current != sentToken){
                 return@synchronized response.request.signWith(current)
             }
@@ -35,7 +33,6 @@ class TokenAuthenticator(
             }.getOrNull()
 
             if(newPair==null){
-                // Refresh başarısız
                 tokenStore.clearBlocking()
                 return@synchronized null
             }
@@ -46,4 +43,19 @@ class TokenAuthenticator(
     }
 
     private fun Request.signWith(accessToken: String): Request = newBuilder().header("Authorization", "Bearer $accessToken").build()
+
+    private fun Response.priorResponseCount() : Int{
+        var count = 0
+        var prior = priorResponse
+        while(prior != null)
+        {
+            count++
+            prior = prior.priorResponse
+        }
+        return count
+    }
 }
+
+// Ana sayfada
+// etkinlikler ve "biletlerim" diye 2 tane tasarım
+// apiden etkinlikleri ve biletleri çekip gösterecek..
